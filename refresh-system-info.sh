6aae6351f36425bc4677384fff996ea9ecddc59b
Johan Rönkkö <ronkko.johan@gmail.com>
2023-01-30 17:50:41 +0100

    System information.

---
+++ refresh-system-info.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+outdir="$HOME/.dotfiles/system-info"
+mkdir -p "$outdir"
+
+function append() {
+  cat /dev/stdin
+  $*
+}
+
+function format() {
+  cat < /dev/stdin | sort | uniq | sed '/^$/d'
+}
+
+# Installed cargo packages.
+ls -l "$CARGO_HOME/bin" | awk '{print $9}' | format > "$outdir/cargo.txt"
+
+# Installed pacman packages.
+pacman -Qe | append pacman -Qm | awk '{print $1}' | format > "$outdir/pacman.txt"
+
+# Enabled systemd services.
+systemctl list-unit-files --state=enabled \
+  | awk '{print $1}' \
+  | awk 'BEGIN{FILTER="\\.(service|timer|socket|path|device|mount|automount|swap|target|snapshot|slice|scope)$"} tolower($NF) ~ FILTER { print $NF }' \
+  | format > "$outdir/systemd.txt"
