7203ba6273e8fde9bc3117863b1f0b3294cc96f7
Johan Rönkkö <ronkko.johan@gmail.com>
2023-02-01 16:35:28 +0100

    Multiple monitors for polybar and bspwm.

--- bspwm/.config/bspwm/autostart.sh
+++ bspwm/.config/bspwm/autostart.sh
@@ -14,7 +14,8 @@ function restart {
 
 restart sxhkd -c "$HOME/.config/bspwm/sxhkd/sxhkdrc"
 restart picom
-run nitrogen --restore
 
 ricedir="$XDG_CONFIG_HOME/bspwm/rices/${RICE:-rose-pine}"
 [[ -f "$ricedir/autostart.sh" ]] && bash "$ricedir/autostart.sh"
+
+run nitrogen --restore
--- bspwm/.config/bspwm/rices/rose-pine/autostart.sh
+++ bspwm/.config/bspwm/rices/rose-pine/autostart.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-function restart {
-  killall $1
-  $@&
-}
-
-dir="$XDG_CONFIG_HOME/bspwm/rices/${RICE:-rose-pine}"
-
 bspc config border_width "0"
 bspc config top_padding "10"
 bspc config bottom_padding "60"
@@ -18,4 +11,20 @@ bspc config left_padding "5"
 bspc config right_padding "5"
 bspc config window_gap "10"
 
-restart polybar -c "$dir/polybar/config.ini"
+# Our connected monitors.
+IFS=" " read -r primaryMonitor secondaryMonitor <<< $(monitors)
+
+# Terminate already running bar instances
+killall -q polybar 2>/dev/null
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+dir="$XDG_CONFIG_HOME/bspwm/rices/${RICE:-rose-pine}"
+
+MONITOR="$primaryMonitor" polybar -c "$dir/polybar/config.ini" main-bar &
+
+if [[ "$secondaryMonitor" != "" ]]; then
+  MONITOR="$secondaryMonitor" polybar -c "$dir/polybar/config.ini" secondary-bar &
+fi
+
