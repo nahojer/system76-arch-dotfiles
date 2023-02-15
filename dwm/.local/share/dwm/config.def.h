3fbdb55e165362f0a27a61983087edec4cd1b2b8
Johan Rönkkö <ronkko.johan@gmail.com>
2023-02-15 21:29:23 +0100

    Change dwm default terminal to alacritty.

--- dwm/.local/share/dwm/config.def.h
+++ dwm/.local/share/dwm/config.def.h
@@ -27,7 +27,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
@@ -57,7 +56,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
