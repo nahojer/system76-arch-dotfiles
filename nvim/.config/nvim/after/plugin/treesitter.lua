0fd0aa007c0ce4fbc367694db8118d9f110fb016
Johan Rönkkö <ronkko.johan@gmail.com>
2023-02-18 11:33:07 +0100

    Prevent treesitter from installing over and over again...

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,4 +1,8 @@
+local parser_install_dir = '$HOME/.local/state/nvim/treesitter'
+vim.opt.runtimepath:append(parser_install_dir)
+
 require('nvim-treesitter.configs').setup {
+  parser_install_dir = parser_install_dir,
   ensure_installed = {
     'bash',
     'css',
