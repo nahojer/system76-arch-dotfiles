da8fde91b410dd8a92041434c81fdd597320edc9
Johan Rönkkö <ronkko.johan@gmail.com>
2023-02-19 11:56:10 +0100

    nvim-cmp ghost text

--- nvim/.config/nvim/after/plugin/cmp.lua
+++ nvim/.config/nvim/after/plugin/cmp.lua
@@ -3,12 +3,12 @@ local luasnip = require 'luasnip'
 
 cmp.setup {
   mapping = {
-    ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-    ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+    ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+    ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<c-y>'] = cmp.mapping(
+    ['<tab>'] = cmp.mapping(
       cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Insert,
         select = true,
@@ -47,6 +47,13 @@ cmp.setup {
         tn = '[TabNine]',
       },
     },
+  },
+  experimental = {
+    -- I like the new menu better! Nice work hrsh7th
+    native_menu = false,
+
+    -- Let's play with this for a day or two
+    ghost_text = true,
   },
 }
 
