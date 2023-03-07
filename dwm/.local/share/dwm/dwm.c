018ee76541f84f5f2f4da37c223758064a229766
Johan Rönkkö <ronkko.johan@gmail.com>
2023-03-07 23:22:58 +0100

    Status text on all monitors

--- dwm/.local/share/dwm/dwm.c
+++ dwm/.local/share/dwm/dwm.c
@@ -973,7 +973,7 @@ drawbar(Monitor *m)
     stw = getsystraywidth();
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon) { /* status is only drawn on selected monitor */
+	if (m == selmon || 1) {
     tw = m->ww - drawstatusbar(m, bh, stext);
 	}
 
@@ -2456,9 +2456,12 @@ updatesizehints(Client *c)
 void
 updatestatus(void)
 {
+  Monitor* m;
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
 		strcpy(stext, "dwm-"VERSION);
-	drawbar(selmon);
+  for(m = mons; m; m = m->next) {
+    drawbar(m);
+  }
   updatesystray();
 }
 
