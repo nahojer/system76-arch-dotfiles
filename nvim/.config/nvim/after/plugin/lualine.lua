497ed41217bbdeaf3dee5290703580e174e15ea3
Johan Rönkkö <ronkko.johan@gmail.com>
2023-03-16 09:05:21 +0100

    Relative path in lualine

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -4,4 +4,14 @@ require('lualine').setup {
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
   },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        file_status = true, -- Displays file status (readonly status, modified status)
+        newfile_status = false, -- Display new file status (new file means no write after created)
+        path = 1, -- 0: Just the filename, 1: Relative path, 2: Absolute path, 3: Absolute path, with tilde as the home directory
+      },
+    },
+  },
 }
